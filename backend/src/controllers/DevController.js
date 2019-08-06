4fbd4793d75bae2651a0c2e11db9dcabcdf1e9f4
dcmunhoz <dc.munhoz@hotmail.com>
2019-08-06 12:31:01 -0300

    D2 - Criando um novo usuário no Mongo.

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,9 +1,41 @@
+const axios = require('axios');
+const Dev   = require('../models/Dev');
+
 // Controller de um Dev
 module.exports = {
 
-    store(req, res) {
+    async store(req, res) {
 
-        return res.json({ok: true});
+        // Parametro passado pelo POST
+        const { username } = req.body;
+
+        // Busca na mongo o usuário informado
+        const userExists = await Dev.findOne({user: username});
+
+        // Verifica se o usuário já existe
+        if (userExists) {
+            
+            // Retorna o usuário caso já exista
+            return res.json(userExists); // END.
+
+        }
+
+        // Consome a API do github para pegar as informações do usuário passado
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+
+        // Extrai as informações necessárias do body
+        const { name, bio, avatar_url: avatar } = response.data;
+
+        // Cria o usuário no mongo
+        const dev = await Dev.create({ 
+            name,
+            user: username,
+            bio,
+            avatar
+         });
+
+        // Retorna o usuário criado.
+        return res.json(dev); // END.
         
     }
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,6 @@
-const express = require('express'); // Express
-const Dev     = require('./controllers/DevController');
-const routes  = express.Router();    // Router express.
+const express       = require('express'); // Express
+const DevController = require('./controllers/DevController');
+const routes        = express.Router();    // Router express.
 
 // Principais metodos HTTP:
 // GET, POST, PUT, DELETE
@@ -9,11 +9,11 @@ routes.get('/', (req, res) => {
 
     let name = req.query.name;
 
-    return res.json({ message: `Helslo ${name}` });
+    return res.json({ message: `Hello ${name}` });
 
 });
 
-routes.post('/devs', Dev.store);
+routes.post('/devs', DevController.store);
 
 
 module.exports = routes;
