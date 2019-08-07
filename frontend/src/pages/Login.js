8d9581193f999ec713400bb3f1fe8ace2ca3e019
dcmunhoz <dc.munhoz@hotmail.com>
2019-08-07 10:08:11 -0300

    D3 - Pagina de login

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Login.css';
+
+import logo from '../assets/logo.svg';
+
+export default function Login() {
+
+    return (
+        <div className="login-container">
+            <form>
+                <img src={logo} alt="Tindev" />
+
+                <input 
+                    placeholder="Digite seu usuário do github"
+                />
+                <button>Enviar</button>
+            </form>
+        </div>
+    );
+
+}
