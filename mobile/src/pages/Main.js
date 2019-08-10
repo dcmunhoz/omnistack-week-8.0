e88f4d07ed22c12e5de5acfb67febb5559ae4c67
Daniel Munhoz <dc.munhoz@hotmail.com>
2019-08-10 13:04:09 -0300

    D4  - Aplicativo mobile finalizado

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,25 +1,121 @@
-import React from 'react';
-import { SafeAreaView, Image, StyleSheet, View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import { SafeAreaView, Image, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+
+import api from '../services/api'
 
 import Logo from '../assets/logo.png'
-import { SourceMapConsumer } from 'source-map';
+import dislike from '../assets/dislike.png'
+import like from '../assets/like.png'
 
-export default function  Main() {
+export default function  Main({navigation}) {
+    const idUser = navigation.getParam('user');
+    const [users, setUsers] = useState([]);
+
+    // const [matchDev, setMatchDev] = useState(null);
+
+    useEffect(() => {
+        async function loadUsers() {
+
+            const response = await api.get('/devs', {
+                headers: { user: idUser }
+            });
+
+            console.log(idUser);
+
+            setUsers(response.data);
+        };   
+
+
+        loadUsers();
+    }, [idUser]);
+
+    useEffect(() => {
+
+        // const socket = io('http://localhost:3333', {
+        //     query: {user: match.params.id}
+        // });
+
+        // socket.on('match', dev => {
+        //     setMatchDev(dev);
+        // }); 
+    
+        // Minha versão para enviar o ID do usuário
+        // socket.emit("user_connected", {
+        //     userId: match.params.id
+        // });
+
+    }, [idUser]);
+
+    async function handleLike(){
+        const [ user, ...rest ] = users;
+
+        await api.post(`/devs/${user._id}/likes`, null, {
+            headers: {
+                user: idUser
+            }
+        });
+
+        setUsers(rest);
+
+    }
+
+    async function handleDislike(){
+        const [ user, ...rest ] = users;
+
+        await api.post(`/devs/${user._id}/dislikes`, null, {
+            headers: {
+                user: idUser
+            }
+        });
+
+        setUsers(rest);
+
+    }
+
+    async function handleLogout(){
+
+        await AsyncStorage.clear();
+
+        navigation.navigate('Login');
+
+    }
+
     return (
         <SafeAreaView style={styles.container}>
-            <Image style={styles.logo} source={Logo}/>
-
+            <TouchableOpacity onPress={handleLogout}>
+                <Image style={styles.logo} source={Logo}/>
+            </TouchableOpacity>
             <View style={styles.cardsContainer}> 
-                <View style={[styles.cards, {zIndex: 3}]}>
-                    <Image style={styles.avatar} source={{uri: 'https://avatars1.githubusercontent.com/u/23061616?v=4'}} />
-                    <View style={styles.footer}>
-                        <Text style={styles.name}> Daniel Munhoz </Text>
-                        <Text style={styles.bio}> Web Developer </Text>
-                    </View>
-                </View>
+                { users.length > 0 ? (
+
+                    users.map((user, index) => (
+                        <View key={user._id} style={[styles.cards, {zIndex: users.length - index}]}>
+                            <Image style={styles.avatar} source={{uri: user.avatar}} />
+                            <View style={styles.footer}>
+                                <Text style={styles.name}> {user.name} </Text>
+                                <Text style={styles.bio} numberOfLines={3}> {user.bio} </Text>
+                            </View>
+                        </View>
+                    ))
+
+                ) : <Text style={styles.empty}>Acabou =[</Text>}            
+
+
             </View>
 
-            <View />
+            { users.length > 0 && (
+
+                <View style={styles.buttonsContainer}> 
+                <TouchableOpacity style={styles.button} onPress={handleDislike}>
+                    <Image source={dislike} /> 
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={handleLike}>
+                    <Image source={like} /> 
+                </TouchableOpacity>
+                </View>
+
+            )}
         </SafeAreaView>
     );
 }
@@ -28,6 +124,13 @@ const styles = StyleSheet.create({
 
     logo: {
         marginTop: 100
+    },
+
+    empty: {
+        alignSelf: 'center',
+        color: '#999',
+        fontSize: 24,
+        fontWeight: 'bold'
     },
 
     container: {
@@ -86,6 +189,28 @@ const styles = StyleSheet.create({
         marginTop: 5,
         lineHeight: 18
 
+    },
+
+    buttonsContainer: {
+        flexDirection: 'row',
+        marginBottom: 30
+    },
+
+    button: {
+        width: 50,
+        height: 50,
+        borderRadius: 50,
+        backgroundColor: '#FFF',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 20,
+        elevation: 2,
+        shadowColor: '#000',
+        shadowOpacity: 0.05,
+        shadowOffset: {
+            width: 0,
+            height: 2
+        }
     }
 
 });
